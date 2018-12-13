ae7622df5bdd2ebb3f7d9590e91cb7a22c5cadcd
GeorgeDUT <zmybsm@qq.com>
2018-12-13 19:05:13 +0800

    decmdp

--- run_2.py
+++ run_2.py
@@ -23,6 +23,7 @@ def cmpt_f(s,x,y):
 
     red_force=[0,0]
     blue_force=[0,0]
+    # cmpt red sum force
     F=0.0
     for i in range(s.red_num):
         # random find a target
@@ -30,32 +31,42 @@ def cmpt_f(s,x,y):
         # i=(i+s.red_num/2)%s.red_num
         end_x,end_y=int(s.red_loc[i][0]),int(s.red_loc[i][1])
         if s.env_map[end_y][end_x]==1:
-            f_x,f_y=force(x,y,end_x,end_y)
-
+            f_x, f_y = force(x, y, end_x, end_y)
+            '''
+            # red force rule 1: sum force is all force
             red_force[0]+=f_x
             red_force[1]+=f_y
-            break
             '''
+
+            # red force rule 2: max force is all force
             F=max(pow(f_x,2)+pow(f_y,2),F)
             if F==pow(f_x,2)+pow(f_y,2):
                 red_force[0],red_force[1]=f_x,f_y
-            '''
         else:
             pass
+    # cmpt blue sum force
     F=0.0
     for i in range(s.blue_num):
         i=random.randint(0,s.blue_num-1)
         end_x,end_y=int(s.blue_loc[i][0]),int(s.blue_loc[i][1])
         if s.env_map[end_y][end_x]==2:
             f_x,f_y=force(x,y,end_x,end_y)
+            '''
+            # blue force rule 1:外力乘以距离的四次方。
             blue_force[0] += f_x*pow((pow(x - end_x, 2) + pow(y - end_y, 2)),2)
             blue_force[1] += f_y*pow((pow(x - end_x, 2) + pow(y - end_y, 2)),2)
             '''
+
+            # blue force rule 2: max force is his all force
             F = max(pow(f_x, 2) + pow(f_y, 2), F)
             if F == pow(f_x, 2) + pow(f_y, 2):
                 blue_force[0], blue_force[1] = f_x, f_y
+
             '''
-            break
+            # blue force rule 3:sum force
+            blue_force[0] += f_x
+            blue_force[1] += f_y
+            '''
         else:
             pass
     return red_force,blue_force
@@ -69,42 +80,85 @@ def brain(s,x,y,team,id):
     action_space = ['u', 'd', 'l', 'r', 's']
     action = np.random.choice(action_space)
     red_f,blue_f=cmpt_f(s,x,y)
-    red_f[0]=red_f[0]+(blue_f[0]*pow(0.1,6))
-    red_f[1] = red_f[1] + (blue_f[1]*pow(0.1,6))
+    r_sum_f,b_sum_f=[0,0],[0,0]
+    # his is blue all sum force-1
+    r_sum_f[0]=red_f[0]+(blue_f[0]*pow(0.1,-1))
+    r_sum_f[1] = red_f[1] + (blue_f[1]*pow(0.1,-1))
+
+    b_sum_f[0] = red_f[0] + (blue_f[0] * pow(0.1, 4))
+    b_sum_f[1] = red_f[1] + (blue_f[1] * pow(0.1, 4))
+
+    # this is blue all sum force-2
+    '''
+    red_f[0] = red_f[0] + (blue_f[0] * pow(0.1, 2.5))
+    red_f[1] = red_f[1] + (blue_f[1] * pow(0.1, 2.5))
+    '''
 
     # choose direction
-    pi = softmax([abs(red_f[0]), abs(red_f[1])])
-    if red_f[0]>0 and red_f[1]>=0:
-        if np.random.rand()<pi[0]:
-            action='r'
-        else:
+    pi_r = softmax([abs(r_sum_f[0]), abs(r_sum_f[1])])
+    pi_b = softmax([abs(b_sum_f[0]), abs(b_sum_f[1])])
+    if team==1:
+        if r_sum_f[0]>0 and r_sum_f[1]>=0:
+            if np.random.rand()<pi_r[0]:
+                action='r'
+            else:
+                action='d'
+        elif r_sum_f[0]<=0<r_sum_f[1]:
+            if np.random.rand() < pi_r[0]:
+                action = 'l'
+            else:
+                action = 'd'
+        elif r_sum_f[0]<0 and r_sum_f[1]<=0:
+            if np.random.rand() < pi_r[0]:
+                action = 'l'
+            else:
+                action = 'u'
+        elif r_sum_f[0]>=0>r_sum_f[1]:
+            if np.random.rand() < pi_r[0]:
+                action = 'r'
+            else:
+                action = 'u'
+        '''
+        elif r_sum_f[0]==0 and r_sum_f[1]>0:
             action='d'
-    elif red_f[0]<=0<red_f[1]:
-        if np.random.rand() < pi[0]:
-            action = 'l'
-        else:
-            action = 'd'
-    elif red_f[0]<0 and red_f[1]<=0:
-        if np.random.rand() < pi[0]:
-            action = 'l'
-        else:
+        elif r_sum_f[0]==0 and r_sum_f[1]<0:
             action = 'u'
-    elif red_f[0]>=0>red_f[1]:
-        if np.random.rand() < pi[0]:
+        elif r_sum_f[0]>0 and r_sum_f[1]==0:
             action = 'r'
-        else:
+        elif r_sum_f[0]<0 and r_sum_f[1]==0:
+            action = 'l'
+        '''
+    elif team==2:
+        if b_sum_f[0] > 0 and b_sum_f[1] >= 0:
+            if np.random.rand() < pi_b[0]:
+                action = 'r'
+            else:
+                action = 'd'
+        elif b_sum_f[0] <= 0 < b_sum_f[1]:
+            if np.random.rand() < pi_b[0]:
+                action = 'l'
+            else:
+                action = 'd'
+        elif b_sum_f[0] < 0 and b_sum_f[1] <= 0:
+            if np.random.rand() < pi_b[0]:
+                action = 'l'
+            else:
+                action = 'u'
+        elif b_sum_f[0] >= 0 > b_sum_f[1]:
+            if np.random.rand() < pi_b[0]:
+                action = 'r'
+            else:
+                action = 'u'
+        '''
+        elif b_sum_f[0]==0 and b_sum_f[1]>0:
+            action='d'
+        elif b_sum_f[0]==0 and b_sum_f[1]<0:
             action = 'u'
-    '''
-    elif red_f[0]==0 and red_f[1]>0:
-        action='d'
-    elif red_f[0]==0 and red_f[1]<0:
-        action = 'u'
-    elif red_f[0]>0 and red_f[1]==0:
-        action = 'r'
-    elif red_f[0]<0 and red_f[1]==0:
-        action = 'l'
-    '''
-
+        elif b_sum_f[0]>0 and b_sum_f[1]==0:
+            action = 'r'
+        elif b_sum_f[0]<0 and b_sum_f[1]==0:
+            action = 'l'
+        '''
     return action
 
 
@@ -117,7 +171,7 @@ def pseudo(my_map):
         for i in range(my_map.red_num):
             x,y=my_map.red_army[i].x,my_map.red_army[i].y
             a=brain(s,x,y,1,i)
-            a=np.random.choice(['u','d','l','r','s'])
+            a=np.random.choice(['s','u','d','l','r'])
             red_action.append(a)
         for i in range(my_map.blue_num):
             x,y=my_map.blue_army[i].x,my_map.blue_army[i].y
