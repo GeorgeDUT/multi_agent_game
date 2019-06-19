d0e82fadcc201b5c1a92dcecae71a0fe9a6dfa83
GeorgeDUT <zmybsm@qq.com>
2019-06-20 00:21:30 +0800

    change

--- run_4.py
+++ run_4.py
@@ -10,11 +10,11 @@ import tensorflow as tf
 # c=tf.matmul(a,b)
 # sess=tf.Session(config=tf.ConfigProto(log_device_placement=True))
 # print(sess.run(c))
-weight_r=[1,-1,0]
-weight_b=[-1,-1,0]
+weight_r=[-1,-0.6,0]
+weight_b=[0,0,0]
 
-MAP_H=10
-MAP_W=10
+MAP_H=11
+MAP_W=11
 
 dis_of_red=[]
 dis_of_blue=[]
@@ -212,7 +212,7 @@ def update():
 
 
 if __name__=="__main__":
-    my_map=WarMap4(MAP_W,MAP_H,5,5,True,True)
+    my_map=WarMap4(MAP_W,MAP_H,10,10,True,False)
     if my_map.draw_pic:
         my_map.after(10,update)
         my_map.mainloop()
